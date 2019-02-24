85330c679c50b26cc7f8ce1d9db7e241bbbda145
Eric Mauldin <emauldin84@gmail.com>
2019-02-24 17:01:03 -0500

    adds new shovel to the findable items. Will reset shovel health.

--- treasure_hunter_pygame.py
+++ treasure_hunter_pygame.py
@@ -43,13 +43,19 @@ class Treasure(pygame.sprite.Sprite):
         self.x_treasure = x_treasure # generate random x coordinate
         self.y_treasure = y_treasure # generate random y coordinate
 
+class Shovel(pygame.sprite.Sprite):
+    def __init__(self, x_shovel, y_shovel):
+        pygame.sprite.Sprite.__init__(self)
+        self.x_shovel = x_shovel # generate random x coordinate
+        self.y_shovel = y_shovel # generate random y coordinate
+
 def main():
     
     pygame.init()
     size =[800, 800]
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
-    fps = 50
+    fps = 60
     bg = pygame.image.load('desert_tileset1.jpg')
     
     font_background = pygame.Surface((800, 46), pygame.SRCALPHA)
@@ -59,22 +65,32 @@ def main():
     end_game_background.fill((0, 0, 0, 128))
 
     broken_shovel = pygame.image.load('broken-shovel.png').convert_alpha()
+    new_shovel_img = pygame.image.load('new_shovel.png').convert_alpha()
     treasure_chest = pygame.image.load('treasure_chest.png').convert_alpha()
     
     pygame.mixer.init()
     sound = pygame.mixer.Sound('Shovel.wav')
     shovel_breaks = pygame.mixer.Sound('LTTP_Shatter.wav')
     victory_sound = pygame.mixer.Sound('WW_Fanfare_Pearl.wav')
+    found_shovel_sound = pygame.mixer.Sound('victory_fanfare.wav')
     victory_sound.set_volume(.5)
     music = pygame.mixer.music.load("Hollow Knight OST - Dung Defender.mp3")
     pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
     pygame.mixer.music.set_volume(.5)
     pygame.mixer.music.play()
     hole = pygame.image.load('hole.png').convert_alpha()
+    
     treasure_coordinates = [-25, 125, 275, 425, 575]
     y_treasure = random.choice(treasure_coordinates)
     x_treasure = random.choice(treasure_coordinates)
     treasure = Treasure(x_treasure, y_treasure)
+
+    # treasure_coordinates.remove(x_treasure)
+    
+    x_shovel = random.choice(treasure_coordinates)
+    y_shovel = random.choice(treasure_coordinates)
+    new_shovel = Shovel(x_shovel, y_shovel)
+    print(new_shovel)
 
     player = Hunter(100, 100) # determines player start location
 
@@ -82,7 +98,10 @@ def main():
     player_group.add(player)
     treasure_group = pygame.sprite.Group()
     treasure_group.add(treasure)
-    print(x_treasure, y_treasure)
+    shovel_group = pygame.sprite.Group()
+    shovel_group.add(new_shovel)
+    print("Treasure is located at:", x_treasure, y_treasure)
+    print("New Shovel is located at:", x_shovel, y_shovel)
 
     
     font = pygame.font.Font(None, 32)
@@ -98,10 +117,13 @@ def main():
     found_treasure_message = font.render("Treasure Hunter has found the Treasure! You win!", True, (255, 255, 255))
     you_lose_message_1 = font.render("Your shovel has broken. You lose!", True, (255, 255, 255))
     play_again_message = font.render("Press 'Y' to start try again. Press 'N' to quit.", True, (255, 255, 255))
-    nothing_there_message = nothing_there_font.render("Nothing there!", True, (255, 255, 255))
+    # nothing_there_message = nothing_there_font.render("Nothing there!", True, (255, 255, 255))
+    found_shovel_message = font.render("You found a new shovel! Shovel health reset.", True, (255, 255, 255))
+    continue_message = font.render("Press 'enter' to continue.", True, (255, 255, 255))
     dig_counter = dig_length
     hole_list = []
     dig_result = ()
+    found_shovel = ()
     
     
     while True:
@@ -135,13 +157,19 @@ def main():
                         dig_result = True
                         pygame.mixer.music.set_volume(.2)
                         victory_sound.play()
+                    
+                        
                     else:
                         shovel_vitals -= 2
                         print(shovel_vitals)
                         dig_result = False
                     if shovel_vitals == 0:
                         sound.set_volume(.0)
-                        shovel_breaks.play()   
+                        shovel_breaks.play()
+                    if (player.rect.x, player.rect.y) == (x_shovel, y_shovel):
+                        found_shovel = True
+                        pygame.mixer.music.set_volume(.2)
+                        found_shovel_sound.play() 
             
         if event.type == pygame.QUIT:
             return False
@@ -158,14 +186,26 @@ def main():
             player.image = pygame.image.load('hunter.png').convert_alpha()
         else:
             player.image = pygame.image.load('hunter-dig.png').convert_alpha()
-            if dig_result == False:
-                current_status = screen.blit(nothing_there_message, (12, 12))
+            # if dig_result == False:
+            #     current_status = screen.blit(nothing_there_message, (12, 12))
         
         player_group.draw(screen)
-
+        
+        if found_shovel == True:
+            shovel_vitals = 12
+            new_shovel = Shovel(0, 0)
+            screen.blit(end_game_background, (150, 345))
+            current_status = screen.blit(found_shovel_message, (190, 350))
+            screen.blit(continue_message, (290, 370))
+            screen.blit(new_shovel_img, (310, 400))
+            pygame.event.clear()
+            event = pygame.event.wait()
+            if hasattr(event, 'key') and event.key == K_RETURN:
+                found_shovel=False
+        
         # draw dig result to screen
         if dig_result == True:
-            screen.blit(end_game_background, (150,345))
+            screen.blit(end_game_background, (150, 345))
             current_status = screen.blit(found_treasure_message, (160, 350))
             screen.blit(play_again_message, (190, 370))
             screen.blit(treasure_chest, (300, 420))
@@ -175,7 +215,6 @@ def main():
                 main()
             elif hasattr(event, 'key') and event.key == N_Key:
                 pygame.quit()
-            
             
 
         # decrement shovel_vitals by 2 on each False dig result
